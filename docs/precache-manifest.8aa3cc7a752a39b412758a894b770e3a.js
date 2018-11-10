411ce63232e19dbc98e6894e79a4e161f929b172
Freya Elisabeth <freyagehrke@gmx.de>
2018-11-10 22:00:36 +0100

    Enable GitHub Pages

---
+++ docs/precache-manifest.8aa3cc7a752a39b412758a894b770e3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe6e911acea0178d5015",
+    "url": "/static/js/main.fe6e911a.chunk.js"
+  },
+  {
+    "revision": "fa92568d88c7bfad485c",
+    "url": "/static/js/1.fa92568d.chunk.js"
+  },
+  {
+    "revision": "fe6e911acea0178d5015",
+    "url": "/static/css/main.7f08aa93.chunk.css"
+  },
+  {
+    "revision": "5fe1a0b3c7e6ee2e94b23f430b99ae38",
+    "url": "/index.html"
+  }
+];

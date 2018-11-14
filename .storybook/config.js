269e4f7c5ca585ce918446c41e721b3df161d442
Freya Elisabeth <freyagehrke@gmx.de>
2018-11-14 18:41:09 +0100

    Install knobs add-on

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,20 @@
-import { configure } from '@storybook/react';
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import GlobalStyle from '../src/GlobalStyle'
+import { Wrapper } from '../src/App'
+import { withKnobs } from '@storybook/addon-knobs'
+
+addDecorator(story => (
+  <Wrapper>
+    <GlobalStyle />
+    {story()}
+  </Wrapper>
+))
+
+addDecorator(withKnobs)
 
 function loadStories() {
-  require('../src/stories');
+  require('../src/stories')
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
